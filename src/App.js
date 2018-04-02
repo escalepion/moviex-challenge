01ca729e2bc1621b3ce10cc8b24c5f1bcc2aa344
Akif Mucurluoglu <akifmucurlu@gmail.com>
2018-04-02 15:02:12 +0300

    add header component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 
 import './scss/main.css';
 
+import Header from './layouts/Header';
+
 class App extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container-fluid">
+        <Header />
         <div>
           {this.props.children}
         </div>
---
+++ src/layouts/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="row">
+            <div className="col-md-10 mx-auto">
+                Header
+            </div>
+        </div>
+    );
+}
+
+export default Header;
