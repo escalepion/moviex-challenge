34d2e2085890d851c94d6848b71279dd73d88015
Akif Mucurluoglu <akifmucurlu@gmail.com>
2018-04-03 12:18:03 +0300

    add title to list page

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Title from '../layouts/Title';
+import Content from '../layouts/Content';
 import Card from '../components/common/Card';
 
 class List extends Component {
@@ -17,32 +19,51 @@ class List extends Component {
             this.renderList();
         }, 2000);
     }
-    renderList(){
-        if(this.props.params.type === 'movie') {
-            this.setState({ list: this.props.movieList.slice(0,21), errorMessage: undefined });
-        }else if(this.props.params.type === 'series') {
-            this.setState({ list: this.props.serieList.slice(0,21), errorMessage: undefined });
-        }else {
+    renderList() {
+        if (this.props.params.type === 'movie') {
+            this.setState({ list: this.props.movieList.slice(0, 21), errorMessage: undefined });
+        } else if (this.props.params.type === 'series') {
+            this.setState({ list: this.props.serieList.slice(0, 21), errorMessage: undefined });
+        } else {
             this.setState({ errorMessage: 'Oops, something went wrong...' });
         }
     }
     mapList() {
         const key = 'Poster Art';
-        return this.state.list.map((item, index)=> {
+        return this.state.list.map((item, index) => {
             return <div key={index} className="col-md-2"><Card imgUrl={item.images[key].url} title={item.title} /></div>
         });
-    } 
-    render () {
-        if(this.state.errorMessage) {
-            return(
-                <div className="row">
-                    <div className="col">{this.state.errorMessage}</div>
+    }
+    render() {
+        let titleContent;
+        const movieType = this.props.params.type;
+        if (movieType === 'movie') {
+            titleContent = 'Popular Movies';
+        } else if (movieType === 'series') {
+            titleContent = 'Popular Series';
+        } else {
+            titleContent = 'Not Fount';
+        }
+        if (this.state.errorMessage) {
+            return (
+                <div>
+                    <Title content={titleContent} />
+                    <Content>
+                        <div className="row">
+                            <div className="col">{this.state.errorMessage}</div>
+                        </div>
+                    </Content>
                 </div>
             );
         }
         return (
-            <div className="row">
-             {this.mapList()}
+            <div>
+                <Title content={titleContent} />
+                <Content>
+                    <div className="row">
+                        {this.mapList()}
+                    </div>
+                </Content>
             </div>
         );
     }
