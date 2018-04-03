4d5699fb49ee20b9f4b2cce898eb0db54b0ff221
Akif Mucurluoglu <akifmucurlu@gmail.com>
2018-04-03 12:22:15 +0300

    styling title

--- src/layouts/Title.js
+++ src/layouts/Title.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Title extends Component {
     render() {
         return (
-            <div className="row">
+            <div className="row title">
                 <div className="col-md-10 mx-auto">{this.props.content}</div>
             </div>
         );
